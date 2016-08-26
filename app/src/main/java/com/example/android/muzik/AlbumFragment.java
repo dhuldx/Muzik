0e541e81de5628ba68411cbdb86b4a58bd515e98
Tiru Dx <tirudx@gmail.com>
2016-08-26 21:30:50 +1000

    first commit

---
+++ app/src/main/java/com/example/android/muzik/AlbumFragment.java
@@ -0,0 +1,30 @@
+package com.example.android.muzik;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.android.miwok.R;
+
+
+public class AlbumFragment extends Fragment{
+
+    public AlbumFragment() {
+        // Required empty public constructor
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_album, container, false);
+    }
+
+}
